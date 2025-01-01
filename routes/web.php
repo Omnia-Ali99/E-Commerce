c805b6e0941bafa67b830d49e3b1d0db50a361ca
Omnia Ali <omniaaliabdalsamia@gmail.com>
2025-01-01 14:27:23 +0200

    add theme & multi-lang

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ 
+
+        Route::get('/test', function () {
+            return view('dashboard.welcome');
+        });
+        
+    });
